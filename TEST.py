be656172989f9888b6db7d8ca69a8438195e9fed
BrianGoldYT <brianldg92@gmail.com>
2020-12-15 01:17:37 +0000

    0

--- TEST.py
+++ TEST.py
@@ -1,18 +1,8 @@
-from getpass import getpass
-import sys,os
+file=open("test1.txt","r")
 
-while True:
-	passwd = getpass("Password: ")
-	passwd2 = getpass("Repeat Password: ")
-	if (passwd == passwd2):
-		print("Acceso permitido")
-		break
-	else:
-		os.system("clear")
-		print("Las contraseñas no coinciden")
+text=file.read()
+text=text.replace("buenas","pepe")
 
-
-parted -s {device} mklabel gpt
-parted -s {device} mkpart efi 0 512
-parted -s {device} mkpart system 512 100%
-
+file.close()
+file=open("test1.txt","w")
+file.write(text)
--- install.py
+++ install.py
@@ -42,13 +42,70 @@ def instalar_sistema_base():
 		os.system("mkdir /mnt/boot/efi")
 		os.system("mount " + part_efi + " /mnt/boot/efi")
 	def particionado_lvm_cifrado(hostname):
-		pass
+		os.system("lsblk -p")
+		print("Indique el disco donde quiere realizar el particionado:")
+		disk = input("> ")
+		os.system(f"parted -s {disk} mklabel gpt") # Crea tabla de particiones GPT
+		os.system(f"parted -s {disk} mkpart efi fat32 0 512") # Crea una particion llamada efi de 512M
+		os.system(f"parted -s {disk} mkpart system ext4 512 100%") # Crea una partición llamada system con el resto del almacenamiento
+		os.system("clear")
+		print("Indicar particiones manualmente?(y/N)")
+		print("* Opcional")
+		opt=input("> ")
+		if((opt =="y") or (opt == "Y")):
+			part_efi=input("Particion EFI: ")
+			part_system=input("Particion System: ")
+			part_home=input("* Particion Home: ")
+			if(part_home):
+				print("Se utilizara disco externo")
+		elif ((opt == "") or (opt == "N") or (opt == "n")):
+			part_efi=disk+"1"
+			part_system=disk+"2"
+		os.system("clear")
+		print("A continuacion se colizitara la contraseña de cifrado del disco")
+		os.system(f"cryptsetup luksFormat --type luks2 {part_system}")
+		os.system(f"cryptsetup open {part_system} enc")
+		os.system("pvcreate /dev/mapper/enc")
+		os.system("vgcreate vol /dev/mapper/enc")
+		os.system("lvcreate -l +100%FREE vol -n root")
+		os.system("mkfs.ext4 /dev/mapper/vol-root")
+
+		os.system("mount /dev/mapper/vol-root /mnt")
+
+		os.system("mkfs.fat " + part_efi)
+		os.system("mkdir /mnt/boot")
+		os.system("mkdir /mnt/boot/efi")
+		os.system("mount " + part_efi + " /mnt/boot/efi")
 	#--------------------------------------------#
 	#----- Funciones de instalación -------------#
 	def instalar_sistema_y_efi():
 		os.system("pacstrap /mnt base linux linux-firmware")
 		os.system("genfstab -U /mnt >> /mnt/etc/fstab")
 		os.system("arch-chroot /mnt pacman --noconfirm -S grub efibootmgr")
+		os.system("arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=ArchLinux --recheck")
+		os.system("arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg")
+	def instalar_sistema_y_efi_lvm_cifrado():
+		os.system("pacstrap /mnt base linux linux-firmware")
+		os.system("genfstab /mnt >> /mnt/etc/fstab")
+
+		file=open("/mnt/etc/mkinitcpio.conf","r")
+		text=file.read()
+		text=text.replace("modconf block filesystems keyboard fsck","modconf block keymap encrypt lvm2 filesystems keyboard fsck")
+		file.close()
+		file=open("/mnt/etc/mkinitcpio.conf","w")
+		file.write(text)
+		os.system("arch-chroot /mnt mkinitcpio -p linux")
+
+		os.system("arch-chroot /mnt pacman --noconfirm -S grub efibootmgr")
+
+		file=open("/mnt/etc/default/grub","r")
+		text=file.read()
+		text=text.replace('GRUB_CMDLINE_LINUX=""',f'GRUB_CMDLINE_LINUX="cryptdevice={part_system}:enc"')
+		file.close()
+		file=open("/mnt/etc/default/grub","w")
+		file.write(text)
+
+
 		os.system("arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=ArchLinux --recheck")
 		os.system("arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg")
 	def configuracion_basica():
@@ -86,5 +143,8 @@ def instalar_sistema_base():
 		configuracion_basica()
 	elif (opt == "2"):
 		print("Instalacion estandar cifrado")
+		particionado_lvm_cifrado()
+		instalar_sistema_y_efi_lvm_cifrado()
+		configuracion_basica()
 
 instalar_sistema_base()
