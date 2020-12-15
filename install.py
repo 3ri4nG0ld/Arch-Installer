54b8c8a7d96ab6ee6ad88a06583b81fa3ee064e2
BrianGoldYT <brianldg92@gmail.com>
2020-12-15 02:26:14 +0000

    Update install.py

--- install.py
+++ install.py
@@ -62,7 +62,7 @@ def instalar_sistema_base():
 			part_system=disk+"2"
 		os.system("clear")
 		print("A continuacion se solizitara la contraseña de cifrado del disco:")
-		subprocess.call("cryptsetup luksFormat --type luks2 "+ part_system,shell=True)
+		os.system("cryptsetup luksFormat --type luks2 /dev/sda2")
 		print(part_system)
 		input("Enter para continuar...")
 		os.system("cryptsetup open " + part_system + " enc")
